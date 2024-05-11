fcbae2c5903d47325c9bfe572afade04124b7acd
Chris Santiago <chrisantiago217@gmail.com>
2024-05-11 13:55:47 -0400

    changes to model and start command

--- src/models/userAnalytics.js
+++ src/models/userAnalytics.js
@@ -1,16 +1,24 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
-const analyticsSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  date: { type: Date, default: Date.now },
-  metrics: {
-    totalApplications: Number,
-    responseRate: Number,
-    interviewRate: Number,
-    offerRate: Number,
+const analyticsSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    metrics: {
+      totalApplications: Number,
+      responseRate: Number,
+      interviewRate: Number,
+      offerRate: Number,
+      applicationStatusBreakdown: Map,
+    },
   },
-})
+  { timestamps: true },
+  { collection: 'Analytics' }
+)
 
-const Analytics = mongoose.model("Analytics", analyticsSchema)
+const Analytics = mongoose.model('Analytics', analyticsSchema)
 
 export default Analytics
